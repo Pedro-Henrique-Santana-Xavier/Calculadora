6f62c198c97d25bb865dff2da25a69f3062be784
Pedro_Santana <phsx016@gmail.com>
2023-09-13 10:56:00 -0300

    Update script.js

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,28 +1,23 @@
 const display = document.querySelector(".display");
-const buttons = dosument.querySelector("button");
-const specialchars = ["%","*","/","+","="];
-
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
 
 const calculate = (btnValue) => {
   display.focus();
-  if(btnValue === "=" && output !== " "){
-
-    output = eval(output.replace("%","/100"));
-
-  }else if(btnValue === "AC"){
-
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
     output = output.toString().slice(0, -1);
-    
-  }else{
-    if (output === "" && specialchars.includes(btnValue)) return;
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
     output += btnValue;
-
   }
   display.value = output;
 };
 
 buttons.forEach((button) => {
-  
-    button.addEventListenser("click",(e) => calculator(e.target.dataset.value));
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
